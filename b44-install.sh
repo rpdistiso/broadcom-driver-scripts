c316d7c588a47091ea5181901b949eb2bdc77536
Blackjay07 <116186005+rpdistiso@users.noreply.github.com>
2024-11-19 11:52:45 -0500

    Update b44-install.sh

--- b44-install.sh
+++ b44-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update && apt-get upgrade
-apt-get install wget firmware-b44-installer wpasupplicant wireless-tools
+apt-get install wget firmware-b43-installer wpasupplicant wireless-tools
 apt remove broadcom-sta-dkms
 modprobe b44
 rm /etc/modprobe.d/broadcom-sta-dkms.conf

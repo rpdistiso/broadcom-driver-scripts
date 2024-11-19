a62aa3f8a2fcfd59167da55d26b19562e4e97a4c
Blackjay07 <116186005+rpdistiso@users.noreply.github.com>
2024-11-19 12:53:04 -0500

    Update b43-install.sh

--- b43-install.sh
+++ b43-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update && apt-get upgrade
-apt-get install wget firmware-b43-installer wpasupplicant
+apt-get install wget firmware-b43-installer firmware-b43legacy-installer wpasupplicant
 apt remove broadcom-sta-dkms
 modprobe b43
 rm /etc/modprobe.d/broadcom-sta-dkms.conf
